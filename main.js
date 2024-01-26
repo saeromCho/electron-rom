d323735015ebd06c914be5cf3b992bf0b6ce70f2
romCho <saerom.r.cho@ent-bc.com>
2024-01-26 22:47:43 +0900

    path require 추가

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     /// 페이지가 표시되기 전에 실행할 전처리 코드를 지정. 스크립트는 반드시 절대 경로로 전달해야한다.
-     webPreferences: { preload: path.join(__dirname, 'preload.js') }
+//     webPreferences: { preload: path.join(__dirname, 'preload.js') }
+     webPreferences: {
+           preload: path.join(__dirname, 'preload.js')
+         }
   })
 
   win.loadFile('index.html')
--- preload.js
+++ preload.js
@@ -1,10 +1,13 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const replaceText = (selector, text) => {
-        const element = document.getElementById(selector)
-        if (element) element.innerText = text
-    }
 
-    for (const type of ['chrome', 'node', 'electron']) {
-        replaceText(`${type}-version`, process.versions[type]);
-    }
-});
+//It's using Node.js 18.18.2, Chromium 120.0.6099.216 and Electron 28.1.4.
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  for (const dependency of ['chrome', 'node', 'electron']) {
+    replaceText(`${dependency}-version`, process.versions[dependency])
+  }
+})
+
