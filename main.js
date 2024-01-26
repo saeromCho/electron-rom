8064559688e00694fb7252bdc715c2c9aeb20a6f
romCho <saerom.r.cho@ent-bc.com>
2024-01-26 22:38:34 +0900

    preload.js 인 전처리 코드 추가

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ const { app, BrowserWindow } = require('electron')
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    /// 페이지가 표시되기 전에 실행할 전처리 코드를 지정. 스크립트는 반드시 절대 경로로 전달해야한다.
+     webPreferences: { preload: path.join(__dirname, 'preload.js') }
   })
 
   win.loadFile('index.html')
@@ -20,6 +22,7 @@ app.whenReady().then(() => {
 })
 
 app.on('window-all-closed', () => {
+console.log('process.platform::', process.platform);
   if (process.platform !== 'darwin') {
     app.quit()
   }
